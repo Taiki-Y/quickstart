5bc8e794e7986784e9451079f76e3dd1b7fadd95
Yamano Taiki <yamanotaiki@YamanonoMac-mini.local>
2020-02-07 19:27:36 +0900

    不要ファイルの削除

--- .history/public/js/tasks_20200205135637.js
+++
@@ -1,7 +0,0 @@
-function delete_alert(event){
-  if(!window.confirm('本当に削除しますか？')){
-    window.alert('キャンセルしました');
-    return false;
-  }
-  document.deleteform.submit();
-};
--- .history/resources/views/show.blade_20200205172048.php
+++
@@ -1,4 +0,0 @@
-@extends('layouts.app')
-@section('content')
-    <p>aa</p>
-    @endsection
